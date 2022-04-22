5543260e3ee2461802e9b1a3d1722d523a3694b7
julehs <giulia.caputo@studenti.unina.it>
2022-04-22 18:10:10 +0200

    TEST MENU E MAIN

---
+++ Exercise2/exercise2/menu2.cpp
@@ -0,0 +1,56 @@
+#include "vector/vector.hpp"
+#include "list/list.hpp"
+#include <iostream>
+
+void menu(){
+  bool quit = false;
+  //int opzione(0);
+  char opzione;
+
+  std::cout<<"Seleziona il tipo di struttura\n\tVettore di interi (1)\tVettore di Float(2)\tVettore di Stringhe(3)\tVettore di Struct(4)\t\n"<< std::endl;
+  std::cout<<"\tLista di interi (5)\tLista di Float(6)\tListe di Stringhe(7)\tLista di Struct(8)\n"<< std::endl;
+  std::cout<<"\t\t\t(premi q per uscire)"<< std::endl;
+  while (opzione != 'q'){
+    std::cin >> opzione;
+
+      if ((opzione !='1') && (opzione != '2')  && (opzione != '3') && (opzione != '4') && (opzione != '5') && (opzione !='6') && (opzione !='7') && (opzione !='8') && (opzione !='q')){
+	std::cout << "ERRORE: scelta non valida\nLe strutture valide sono i Vettori(1-2-3) e le Liste(4-5-6)\n";
+	}
+
+      if (opzione == '1'){
+	VectorInt();
+      }
+      if (opzione == '2'){
+	VectorFloat();
+	quit = true;
+      }
+      if (opzione == '3'){
+	VectorString();
+	quit = true;
+      }
+      if (opzione == '4'){
+	VectorStruct();
+	quit = true;
+      }
+      if (opzione == '5'){
+	ListInt();
+	quit = true;
+      }
+      if (opzione == '6'){
+	ListFloat();
+	quit = true;
+      }
+      if (opzione == '7'){
+	ListString();
+	quit = true;
+      }
+      if (opzione == '8'){
+	ListStruct();
+	quit = true;
+      }
+      if (opzione == 'q'){
+	std::cout<< "Goodbye!"<< std::endl;
+	quit = true;
+      }
+    }
+}
--- Exercise2/exercise2/zmytest/test.cpp
+++ Exercise2/exercise2/zmytest/test.cpp
@@ -1,2 +1,35 @@
+#include "test.hpp"
+#include <iostream>
 
-// ...
+void MapPrintPosition(pos& dat, void* _){
+	std::cout << "{" << dat.x << " " << dat.y << "}\n";
+}
+
+void MapDouble(int& dat, void* _) {
+  dat *= 2;
+}
+
+void MapSquare(float& dat, void* _) {
+  dat *= dat;
+}
+
+void MapToUpper(std::string& dat, void* _){
+	for (auto& c: dat)
+		c = toupper(c);
+}
+
+void FoldAdd(const int& dat, const void* n, void* acc) {
+	if(dat < *((int*)n))
+		*((int*) acc) += dat;
+}
+
+void FoldMultiply(const float& dat, const void* n, void* acc) {
+	if(dat < *((int*)n))
+		*((float*) acc) *= dat;
+}
+
+void FoldConcat(const std::string& dat, const void* n, void* acc) {
+	if(dat.length() <= *((int*)n))
+		((std::string*) acc)->append(dat);
+}
+
