3870dde7346cf7eee371033c5fd111f439adafb7
julehs <giulia.caputo@studenti.unina.it>
2022-05-05 12:22:49 +0200

    inizio binarytree.cpp

--- Exercise3/exercise3/binarytree/binarytree.cpp
+++ Exercise3/exercise3/binarytree/binarytree.cpp
@@ -5,7 +5,41 @@ namespace lasd {
 
 /* ************************************************************************** */
 
-// ...
+
+//Node
+
+
+//Comparision operators
+template <typename Data>
+bool BinaryTree<Data>::Node::operator==(const Node &compareNode) const noexcept{
+
+    if (compareNode.Element()== this->Element()){
+        bool thisRC = this->HasRightChild();
+        bool thisLC = this->HasLeftChild();
+
+        bool hisRC = compareNode.HasRightChild();
+        bool hisLF = compareNode.HasLeftChild();
+
+        bool right = (thisRC == thisLC);
+        bool left = (thisLC == thisRC);
+
+        if(left && right){
+            if(thisLC && thisRC){
+                return ((compareNode.LeftChild()== LeftChild()) && (compareNode.RightChild()==RightChild()));
+            }
+            else if(thisLC){
+                return(compareNode.LeftChild()== LeftChild());
+            }
+            else if(thisRC){
+                return(compareNode.RightChild()==RightChild()); 
+            }else{
+                return true;
+            }
+
+        }
+    }
+    return false;
+}
 
 /* ************************************************************************** */
 
