3289aed5fa4ec6cd906cc6a956a52eab3882fe24
julehs <giulia.caputo@studenti.unina.it>
2022-05-05 18:38:17 +0200

    AUX func incompleto

--- Exercise3/exercise3/binarytree/binarytree.cpp
+++ Exercise3/exercise3/binarytree/binarytree.cpp
@@ -69,7 +69,109 @@ bool BinaryTree<Data>::operator!=(const BinaryTree<Data> &compareBT) const noexc
 }
 
 
+//SPECIFIC FUNCTION
 
+//Specific member function
+//Map,Fold,MapPreOrder,FoldPreOrder,MapPostOrder,FoldPostOrder,MapInOrder,FoldInOrder,MapBreadth,FoldBreadth
+
+template <typename Data>
+void BinaryTree<Data>::Map(MapFunctor fun, void* par){
+    if(dim!=0)
+        Map(fun,par,&(Root()));
+
+}
+
+template <typename Data>
+void BinaryTree<Data>::Fold(FoldFunctor fun, const void* par, void* acc) const{
+    if(dim!=0)
+        Fold(fun,par,acc,&(Root()));
+
+}
+
+
+template <typename Data>
+void BinaryTree<Data>::MapPreOrder(MapFunctor fun, void* par){
+    if (dim!=0)
+        MapPreOrder(fun, par, &(Root()));
+}
+
+template <typename Data>
+void BinaryTree<Data>::FoldPreOrder(FoldFunctor fun, const void* par, void* acc) const{
+    if(dim!=0)
+        FoldPreOrder(fun,par,acc,&(Root()));
+}
+
+template <typename Data>
+void BinaryTree<Data>::MapPostOrder(MapFunctor fun, void* par){
+    if(dim!=0)
+        MapPostOrder(fun,par, &(Root()));
+}
+
+template <typename Data>
+void BinaryTree<Data>::FoldPostOrder(FoldFunctor fun, const void* par, void* acc) const{
+    if(dim!=0)
+        FoldPostOrder(fun, par, acc, &(Root()));
+}
+
+template <typename Data>
+void BinaryTree<Data>::MapInOrder(MapFunctor fun, void* par){
+    if(size!=0)
+        MapInOrder(fun, par, &(Root()));
+}
+
+template <typename Data>
+void BinaryTree<Data>::FoldInOrder(FoldFunctor fun, const void* par, void* acc) const{
+    if(dim!=0)
+        FoldInOrder(fun, par, acc, &(Root()));
+}
+
+
+template <typename Data>
+void BinaryTree<Data>::MapBreadth(MapFunctor fun, void* par){
+    if(dim!=0)
+        MapBreadth(fun, par, &(Root()));
+}
+
+
+template <typename Data>
+void BinaryTree<Data>::FoldBreadth(FoldFunctor fun, const void* par, void* acc)const{
+    if(dim!=0)
+        FoldBreadth(fun, par, acc, &(Root()));
+}
+
+//AUXILIARY FUNCTIONS
+
+//MapPreOrder,FoldPreOrder,MapPostOrder,FoldPostOrder,MapInOrder,FoldInOrder,MapBreadth,FoldBreadth
+
+template <typename Data>
+void BinaryTree<Data>::MapPreOrder(MapFunctor fun, void* par, Node* node){
+    if(node!=nullptr){
+        fun(node->Element(), par);
+
+
+        if(node->HasLeftChild())
+            MapPreOrder(fun, par, &(node->LeftChild()));
+
+
+        if(node->HasRightChild())
+            MapPreOrder(fun, par, &(node->RightChild()));
+
+    }
+}
+
+
+template<typename Data>
+void BinaryTree<Data>::FoldPreOrder(FoldFunctor fun, const void* par, void*)const{
+    if(node!=nullptr){
+        fun(node->Element(),par,acc);
+
+        if(node->HasLeftChild())
+            FoldPreOrder(fun, par, acc, &(node->LeftChild()));
+
+        if (node->HasRightChild())
+            FoldPreOrder(fun, par, acc, &(node->RightChild()));
+    }
+}
 /* ************************************************************************** */
 
 }
