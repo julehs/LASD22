09b2d3503b16e84f6727bbc6a9edf6963b09fe87
julehs <giulia.caputo@studenti.unina.it>
2022-05-24 14:01:14 +0200

    continuo hash

--- Exercise5/exercise5/hashtable/hashtable.hpp
+++ Exercise5/exercise5/hashtable/hashtable.hpp
@@ -20,7 +20,7 @@ template <typename Data>
 class Hash {
 
 public:
-  // ulong operator() (const Data&) const noexcept;
+   ulong operator() (const Data&) const noexcept;
   // type operator()(argument) specifiers; // (concrete function should not throw exceptions)
 
 };
@@ -38,26 +38,35 @@ private:
 
 protected:
 
-  // using DictionaryContainer<Data>::dim;
+   using DictionaryContainer<Data>::dim;
 
+  //TODO aggiunta di un hash<data>
   // ...
 
 public:
 
   // Destructor
+
+    virtual ~HashTable() = default;
   // ~HashTable() specifiers
 
   /* ************************************************************************ */
 
   // Copy assignment
+
+   HashTable& operator = (const HashTable&) = delete; 
   // type operator=(argument); // Copy assignment of abstract types should not be possible.
 
   // Move assignment
+    HashTable& operator = (HashTable&&) noexcept = delete;
   // type operator=(argument); // Move assignment of abstract types should not be possible.
 
   /* ************************************************************************ */
 
   // Comparison operators
+
+    bool operator == (const HashTable&) const noexcept = delete;
+    bool operator != (const HashTable&) const noexcept = delete;
   // type operator==(argument) specifiers; // Comparison of abstract binary tree is possible.
   // type operator!=(argument) specifiers; // Comparison of abstract binary tree is possible.
 
@@ -65,6 +74,7 @@ public:
 
   // Specific member function
 
+    void Resize(const ulong);
   // type Resize(argument) specifiers; // Resize the hashtable to a given size
 
 protected:
