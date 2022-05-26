94d6ef774071b335db86bad7378d3ceb4d7189ed
julehs <giulia.caputo@studenti.unina.it>
2022-05-26 11:37:11 +0200

    completo close addres hpp

--- Exercise5/exercise5/hashtable/clsadr/htclsadr.hpp
+++ Exercise5/exercise5/hashtable/clsadr/htclsadr.hpp
@@ -14,7 +14,7 @@ namespace lasd {
 /* ************************************************************************** */
 
 template <typename Data>
-class HashTableClsAdr { // Must extend HashTable<Data>
+class HashTableClsAdr: virtual public HashTable<Data> { // Must extend HashTable<Data>
 
 private:
 
@@ -22,18 +22,24 @@ private:
 
 protected:
 
-  // using HashTable<Data>::???;
+   using HashTable<Data>::dim;
 
   // ...
 
 public:
 
   // Default constructor
-  // HashTableClsAdr() specifiers;
+    HashTableClsAdr() = default;
 
   /* ************************************************************************ */
 
   // Specific constructors
+
+    HashTableClsAdr(const ulong);
+
+    HashTableClsAdr(const LinearContainer<Data>&);
+
+    HashTableClsAdr(const ulong LinearContainer<Data>&);
   // HashTableClsAdr(argument) specifiers; // A hash table of a given size
   // HashTableClsAdr(argument) specifiers; // A hash table obtained from a LinearContainer
   // HashTableClsAdr(argument) specifiers; // A hash table of a given size obtained from a LinearContainer
@@ -41,27 +47,37 @@ public:
   /* ************************************************************************ */
 
   // Copy constructor
+
+    HashTableClsAdr (const HashTableClsAdr&);
   // HashTableClsAdr(argument) specifiers;
 
+
+
   // Move constructor
+
+    HashTableClsAdr (HashTableClsAdr&&) noexcept;
   // HashTableClsAdr(argument) specifiers;
 
   /* ************************************************************************ */
 
   // Destructor
-  // ~HashTableClsAdr() specifiers;
+   virtual ~HashTableClsAdr() = default; ;
 
   /* ************************************************************************ */
 
   // Copy assignment
+    HashTableClsAdr& operator = (const HashTableClsAdr&) = delete;
   // type operator=(argument) specifiers;
 
   // Move assignment
+    HashTableClsAdr& operator = (HashTableClsAdr&&) noexcept = delete;
   // type operator=(argument) specifiers;
 
   /* ************************************************************************ */
 
   // Comparison operators
+    bool operator == (const HashTableClsAdr&) const noexcept;
+    bool operator != (const HashTableClsAdr&) const noexcept;
   // type operator==(argument) specifiers;
   // type operator!=(argument) specifiers;
 
@@ -69,7 +85,8 @@ public:
 
   // Specific member functions (inherited from HashTable)
 
-  // type Resize(argument) specifiers; // Resize the hashtable to a given size
+  // type Resize(argument) specifiers; // Resize the hashtable to a given size  
+    void Resize(const ulong);
 
   /* ************************************************************************ */
 
@@ -79,29 +96,37 @@ public:
   // type Insert(argument) specifiers; // Override DictionaryContainer member (Move of the value)
   // type Remove(argument) specifiers; // Override DictionaryContainer member
 
+    virtual void Insert(const Data&) override;
+    virtual void Insert(Data&&) noexcept override;
+    virtual void Remove(consrt Data&) override;
+
   /* ************************************************************************ */
 
   // Specific member functions (inherited from TestableContainer)
 
   // type Exists(argument) specifiers; // Override TestableContainer member
+    virtual bool Exists(const Data&) const noexcept override;
 
   /* ************************************************************************ */
 
   // Specific member functions (inherited from MappableContainer)
 
   // type Map(argument) specifiers; // Override MappableContainer member
+    virtual void Map(MapFunctor, void*) override;
 
   /* ************************************************************************ */
 
   // Specific member functions (inherited from FoldableContainer)
 
   // type Fold(argument) specifiers; // Override FoldableContainer member
+    virtual void Fold(FoldFunctor, const void*, void* ) const override;
 
   /* ************************************************************************ */
 
   // Specific member functions (inherited from Container)
 
   // type Clear() specifiers; // Override Container member
+    virtual void Clear() override;
 
 };
 
