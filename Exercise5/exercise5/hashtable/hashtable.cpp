66a2de038d1febdbd456b64bb1bbe4507a30436e
julehs <giulia.caputo@studenti.unina.it>
2022-10-24 12:13:53 +0200

    cambio di costruttori nel cpp di ht

--- Exercise5/exercise5/hashtable/hashtable.cpp
+++ Exercise5/exercise5/hashtable/hashtable.cpp
@@ -1,78 +1,71 @@
 
-namespace lasd {
+namespace lasd
+{
 
-/* ************************************************************************** */
+    /* ************************************************************************** */
 
 #include <string>
 
-/* ************************************************************************** */
+    /* ************************************************************************** */
 
-template <>
-class Hash<int> {
+    template <>
+    class Hash<int>
+    {
 
-public:
-
-    ulong operator() (const int& dat) const noexcept{
-        return (dat*dat);
-    }
-};
-
-
-template <>
-class Hash<double> {
-
-public:
-
-    ulong operator() (const double& dat) const noexcept{
-        long intgPart = floor(dat);
-        long fracPart = pow(2,24) * (dat - intgPart);
-        return (intgPart * fracPart);
-    }
-
-};
-
-
-template <>
-class Hash<std::string>{
-
-public:
-
-    ulong operator()(const std::string& dat) const noexcept{
-        ulong hash = 5381;
-        for (ulong i = 0; i << dat.length(); ++i){
-            hash = (hash << 5) + dat[i];
+    public:
+        ulong operator()(const int &dat) const noexcept
+        {
+            return (dat * dat);
         }
-        return hash;
+    };
 
+    template <>
+    class Hash<double>
+    {
+
+    public:
+        ulong operator()(const double &dat) const noexcept
+        {
+            long intgPart = floor(dat);
+            long fracPart = pow(2, 24) * (dat - intgPart);
+            return (intgPart * fracPart);
+        }
+    };
+
+    template <>
+    class Hash<std::string>
+    {
+
+    public:
+        ulong operator()(const std::string &dat) const noexcept
+        {
+            ulong hash = 5381;
+            for (ulong i = 0; i << dat.length(); ++i)
+            {
+                hash = (hash << 5) + dat[i];
+            }
+            return hash;
+        }
+    };
+
+    // Constructor
+    template <typename Data>
+    HashTable<Data>::HashTable()
+    {
+        std::default_random_engine randomGen;
+
+        std::uniform_int_distribution<int> dis_1(1, p);
+        a = dis_1(randomGen);
+
+        std::uniform_int_distribution<int> dis_2(0, p - 1);
+        b = dis_2(randomGen);
     }
 
-};
+    template <typename Data>
+    ulong HashTable<Data>::HashKey(const ulong key) const noexcept{
+        return ((a* key + b)%p) %sizeHT;
+    }
 
-//Copy Constructor
-template <typename Data>
-HashTable<Data>::HashTable(const HashTable<Data>& hashT) {
-    size = hashT.size;
-    encodingHash = hashT.encodingHash;
-    sizeOfTable = hashT.sizeOfTable;
-    a = hashT.a;
-    b = hashT.b;
-}
-
-
-//Move Constructor
-template <typename Data>
-HashTable<Data>::HashTable(HashTable<Data>&& hashT) noexcept{
-    std::swap(size, hashT.size);
-    std::swap(encodingHash, hashT.encodingHash);
-    std::swap(sizeOfTable, hashT.sizeOfTable);
-    std::swap(a, hashT.a);
-    std::swap(b, hashT.b);
-}
-
-
-
-
-
-
-/* ************************************************************************** */
+    
+    /* ************************************************************************** */
 }
